f6a2673dc6a83aae6b4b3d0055b9677db0b6377f
Luke Brown <me@luke.sx>
2016-08-25 00:30:06 +0100

    Submit Base Code

---
+++ assets/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/black-tie.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+
+	<title>Basecamp</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require_once("assets/header.php"); ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Hello</h1>
+		</div>
+	</div>
+</div>
+
+<?php require_once("assets/footer.php"); ?>
