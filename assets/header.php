f7a5948cd28dc7500fe3b8ee83a2bb2ddf25b36c
Luke Brown <me@luke.sx>
2016-09-11 22:31:40 +0100

    Added fonts

--- assets/header.php
+++ assets/header.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:100,400,300,700' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/black-tie.min.css">
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<h1>Hello</h1>
+			<h1><i class="btl bt-flag"></i> If you're seeing this, then your project is ready to go</h1>
 		</div>
 	</div>
 </div>
