c0528ed8c097dbfcdb1c2892764fcc1065c61c84
Luke Brown <me@luke.sx>
2016-10-13 11:44:37 +0100

    Added favicon

--- assets/header.php
+++ assets/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:100,400,300,700' rel='stylesheet' type='text/css'>
-
+	<link rel="icon" ype="image/png" href="assets/img/logo.png">
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/black-tie.min.css">
 	<link rel="stylesheet" href="assets/css/style.css">
