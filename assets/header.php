881b9b6d92211f0b72af1367348b2b2c45fbd3ed
Luke Brown <me@luke.sx>
2016-10-13 12:02:49 +0100

    Added Bootstrap and jQuery JS files

--- assets/header.php
+++ assets/header.php
@@ -5,11 +5,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:100,400,300,700' rel='stylesheet' type='text/css'>
-	<link rel="icon" ype="image/png" href="assets/img/logo.png">
+	<link rel="icon" type="image/png" href="assets/img/logo.png">
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/black-tie.min.css">
 	<link rel="stylesheet" href="assets/css/style.css">
 
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	
 	<title>Basecamp</title>
 </head>
 <body>
